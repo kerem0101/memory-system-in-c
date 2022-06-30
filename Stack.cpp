31cf987c828c3c3a4e604edb9d6ef764f2936309
Aziz Kerem Demir <demirazizkerem@gmail.com>
2022-06-30 17:12:03 +0300

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+void fun(){
+	
+	int var; //local variable stored in Stack when function is called
+}
+
 int main() {
    
 	int l_var; //local variable stored in Stack
+	
+	fun();
 	
 	return 0;
 }
